bd08ac37e598d4ab4ad8e7dc5062baacff43cd9b
cody <qudgus1984@naver.com>
2023-02-28 16:15:10 +0900

    [잘라서 배열로 저장하기]

--- Level 0/Level 0.playground/Contents.swift
+++ Level 0/Level 0.playground/Contents.swift
@@ -1,5 +1,5 @@
 import Foundation
-
+/*
 //MARK: 이진수 더하기
 func solution(_ bin1:String, _ bin2:String) -> String {
     return String(Int(bin1, radix: 2)! + Int(bin2, radix: 2)!, radix: 2)
@@ -30,5 +30,23 @@ func solution(_ numbers:[Int], _ k:Int) -> Int {
 func solution(_ numbers:[Int], _ k:Int) -> Int {
     return numbers[((k-1)*2) % numbers.count]
 }
+*/
 
+//MARK: 잘라서 배열로 저장하기
+func solution(_ my_str:String, _ n:Int) -> [String] {
+    var answer = ""
+    var arr: [String] = []
+    for i in my_str {
+        answer += String(i)
+        if answer.count == n {
+            arr.append(answer)
+            answer = ""
+        }
+    }
+    
+    if !answer.isEmpty {
+        arr.append(answer)
+    }
+    return arr
+}
 
