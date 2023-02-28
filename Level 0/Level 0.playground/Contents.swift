8695e2d0477d7e264eba67da75ec338b97a286e8
cody <qudgus1984@naver.com>
2023-02-28 15:24:31 +0900

    [공 던지기]

--- Level 0/Level 0.playground/Contents.swift
+++ Level 0/Level 0.playground/Contents.swift
@@ -1,11 +1,11 @@
 import Foundation
 
-//이진수 더하기
+//MARK: 이진수 더하기
 func solution(_ bin1:String, _ bin2:String) -> String {
     return String(Int(bin1, radix: 2)! + Int(bin2, radix: 2)!, radix: 2)
 }
 
-//k의 개수
+//MARK: k의 개수
 func solution(_ i:Int, _ j:Int, _ k:Int) -> Int {
     var x = ""
     for z in i...j {
@@ -13,3 +13,22 @@ func solution(_ i:Int, _ j:Int, _ k:Int) -> Int {
     }
     return Array(x).filter { String($0) == String(k)}.count
 }
+
+//MARK: 공던지기
+func solution(_ numbers:[Int], _ k:Int) -> Int {
+    var x = 0
+    for i in 0...k - 2 {
+        x += 2
+        if x > numbers.count - 1 {
+            x -= numbers.count
+        }
+    }
+    return numbers[x]
+}
+
+//쉬운풀이
+func solution(_ numbers:[Int], _ k:Int) -> Int {
+    return numbers[((k-1)*2) % numbers.count]
+}
+
+
