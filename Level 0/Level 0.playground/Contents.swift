f025884562a2bca0d4b8d001dd3eb4044d6349a4
cody <qudgus1984@naver.com>
2023-02-28 14:44:06 +0900

    [k의 개수]

--- Level 0/Level 0.playground/Contents.swift
+++ Level 0/Level 0.playground/Contents.swift
@@ -1,6 +1,15 @@
 import Foundation
 
+//이진수 더하기
 func solution(_ bin1:String, _ bin2:String) -> String {
     return String(Int(bin1, radix: 2)! + Int(bin2, radix: 2)!, radix: 2)
 }
 
+//k의 개수
+func solution(_ i:Int, _ j:Int, _ k:Int) -> Int {
+    var x = ""
+    for z in i...j {
+        x += "\(z)"
+    }
+    return Array(x).filter { String($0) == String(k)}.count
+}
