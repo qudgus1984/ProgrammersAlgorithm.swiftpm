419652eb64391bd0383dde53cd78fa96fe06aaf6
cody <qudgus1984@naver.com>
2023-03-13 16:45:02 +0900

    [영어가 싫어요]

--- Level 0/Level 0.playground/Contents.swift
+++ Level 0/Level 0.playground/Contents.swift
@@ -311,7 +311,6 @@ func solution(_ common:[Int]) -> Int {
 func solution(_ my_string:String) -> Int {
     return my_string.split(whereSeparator: { !$0.isNumber }).reduce(0) { $0 + Int($1)! }
 }
-*/
 
 func solution(_ myString: String) -> String {
     var uniqueChars = ""
@@ -322,3 +321,11 @@ func solution(_ myString: String) -> String {
     }
     return uniqueChars
 }
+
+func solution(_ numbers:String) -> Int64 {
+
+    return Int64(numbers.replacingOccurrences(of: "zero", with: "0").replacingOccurrences(of: "one", with: "1").replacingOccurrences(of: "two", with: "2").replacingOccurrences(of: "three", with: "3").replacingOccurrences(of: "four", with: "4").replacingOccurrences(of: "five", with: "5").replacingOccurrences(of: "six", with: "6").replacingOccurrences(of: "seven", with: "7").replacingOccurrences(of: "eight", with: "8").replacingOccurrences(of: "nine", with: "9") ) ?? 0
+
+}
+
+ */
